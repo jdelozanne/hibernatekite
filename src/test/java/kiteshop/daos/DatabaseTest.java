12b0a9ef806a06464f736d087f3cc194c776fd2e
SteefP <s.pelgrom@hotmail.com>
2017-10-13 16:50:56 +0200

    KLantDAOtest ook ok

--- src/test/java/kiteshop/daos/DatabaseTest.java
+++ src/test/java/kiteshop/daos/DatabaseTest.java
@@ -142,8 +142,10 @@ public class DatabaseTest {
 
 		// Prepare the SQL statements to insert the test data into the DATABASE
 		String insert_account = "INSERT INTO `" + DATABASE + "`.`account` (`gebruikersnaam`, `wachtwoord`) VALUES ('Steef', 'Cola');";
-		String insert_product = "INSERT INTO `" + DATABASE + "`.`product` (`idProduct`, `productnaam`, `omschrijving`, `prijs`) VALUES ('1', 'Cabrinha Drifter', 'Cabrinha Drifter 2017 Kite Only Red/Blue - 4,5 meter', '719.00'), ('2', 'Cabrinha Chaos', 'Cabrinha Chaos 2017 Kite Only Yellow/Orange - 5,5 meter', '719.00'),('3', 'Brunotti Virtuoso', 'Brunotti Dimension Kiteboard 136 (model 2015)', '399.00');";
-		String insert_klant = "INSERT INTO `" + DATABASE + "`.`klant` (`KlantID`, `voornaam`, `achternaam`, `emailadres`, `straatnaam`, `huisnummer`, `postcode`, `plaats`, `telefoonnummer`) VALUES ('1', 'Steef', 'Pelgrom', 'stevey@hotmail.com', 'Hendriklaan', '38', '5034KL', 'Tilburg', '06-56847965');";
+		String insert_product = "INSERT INTO `" + DATABASE + "`.`product` (`productID`, `productnaam`, voorraad, `prijs`) VALUES ('1', 'Cabrinha Drifter', 5, '719.00'), ('2', 'Cabrinha Chaos', 8,  '719.00'),('3', 'Brunotti Virtuoso', 7, '399.00');";
+		String insert_klant = "INSERT INTO `" + DATABASE + "`.`klant` (`KlantID`, `voornaam`, `achternaam`, emailadres,`telefoonnummer`) VALUES ('1', 'Steef', 'Pelgrom', 'stevey@hotmail.com', '06-56847965');";
+		
+		
 		String insert_bestelling = "INSERT INTO `" + DATABASE + "`.`bestelling` (`bestellingID`, `klantID`) VALUES ('1', '1');";
 		String insert_bestelregel = "INSERT INTO `" + DATABASE + "`.`bestel_regel` (`bestel_regelID`, `aantal`, `Bestelling_bestellingID`, `product_idProduct`) VALUES ('1', '4', '1', '2'), ('2', '1', '1', '2');";
 
