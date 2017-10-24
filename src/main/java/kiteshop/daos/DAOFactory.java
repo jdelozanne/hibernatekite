f87bfb0559f4fcec1e5912f1bcf0c68d951016cf
Unknown <jdelozanne@gmail.com>
2017-10-24 13:09:54 +0200

    dit zou een mogelijkheid zijn voor factory
    1 algemene dao factory, per createmethod een switch tussen sql en mongo

--- src/main/java/kiteshop/daos/DAOFactory.java
+++ src/main/java/kiteshop/daos/DAOFactory.java
@@ -9,21 +9,74 @@ package kiteshop.daos;
  *
  * @author julia
  */
-public abstract class DAOFactory {
+public class DAOFactory {
     
-	
+private KlantDAOInterface klant;
+private AccountDAOInterface account;
+private ProductDAOInterface product;
+private BestellingDAOInterface bestelling;
+private BestelRegelDAOInterface bestelregel;
+
 	
     
-  private void createDao(String database){
+  public KlantDAOInterface createKlantDAO(String database){
 		switch(database.toLowerCase()){
 		case "mysql":
-			new SQLDAOFactory();
+			this.klant = new KlantDAO();
                 case "mongodb":
+                    this.klant = new KlantDAOMongo();
 		default:
-			new MongoDAOFactory();
 		}
+                return klant;
 	}
+  
+  public AccountDAOInterface createAccountDAO(String database){
+      switch(database.toLowerCase()){
+		case "mysql":
+			this.account = new AccountDAO();
+                case "mongodb":
+                    this.account = new AccountDAOMongo();
+		default:
+		}
+                return account;
+	}
+  public ProductDAOInterface createProductDAO(String database){
+      switch(database.toLowerCase()){
+		case "mysql":
+			this.product= new ProductDAO();
+                case "mongodb":
+                    this.product = new ProductDAOMongo();
+		default:
+		}
+                return product;
+	}
+  
+  public BestellingDAOInterface createBestellingDAO(String database){
+      switch(database.toLowerCase()){
+		case "mysql":
+			this.bestelling= new BestellingDAO();
+                case "mongodb":
+                    this.bestelling = new BestellingDAOMongo();
+		default:
+		}
+                return bestelling;
+	}
+  public BestelRegelDAOInterface createBestelRegelDAO(String database){
+      switch(database.toLowerCase()){
+		case "mysql":
+			this.bestelregel= new BestelRegelDAO();
+                case "mongodb":
+                    this.bestelregel = new BestelRegelDAOMongo();
+		default:
+		}
+                return bestelregel;
+	}
+  
+      
+  }
+  
+  
 
        
-   }
+   
 
