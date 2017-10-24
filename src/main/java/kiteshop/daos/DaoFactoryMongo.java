485f38acc4dbde4e5e0a322d2893ad75d50d2a37
SteefP <s.pelgrom@hotmail.com>
2017-10-24 18:42:12 +0200

    mogno factory

---
+++ src/main/java/kiteshop/daos/DaoFactoryMongo.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kiteshop.daos;
+
+/**
+ *
+ * @author Steef P
+ */
+public class DaoFactoryMongo implements DaoFactoryInterface {
+
+	@Override
+	public AccountDaoInterface createAccountDao() {
+		return new AccountDaoMongo();
+	}
+
+	@Override
+	public BestellingDaoInterface createBestellingDao() {
+
+		return new BestellingDaoMongo();
+	}
+
+	@Override
+	public BestelRegelDaoInterface createBestelregelDao() {
+		return new BestelRegelDaoMongo();
+	}
+
+	@Override
+	public KlantDaoInterface createKlantDao() {
+		return new KlantDaoMongo();
+	}
+
+	@Override
+	public ProductDaoInterface createProductDao() {
+		return new ProductDaoMongo();
+	}
+
+}
