610cdad5eb2a342cce1a86db135f9b1175d3dd8e
SteefP <s.pelgrom@hotmail.com>
2017-10-24 18:43:27 +0200

    sql factory

---
+++ src/main/java/kiteshop/daos/DaoFactorySql.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kiteshop.daos;
+
+/**
+ *
+ * @author Steef P
+ */
+public class DaoFactorySql implements DaoFactoryInterface {
+
+	@Override
+	public AccountDaoInterface createAccountDao() {
+		
+		return new AccountDaoSql();
+	}
+
+	@Override
+	public BestellingDaoInterface createBestellingDao() {
+		// TODO Auto-generated method stub
+		return new BestellingDaoSql();
+	}
+
+	@Override
+	public BestelRegelDaoInterface createBestelregelDao() {
+		// TODO Auto-generated method stub
+		return new BestelRegelDaoSql();
+	}
+
+	@Override
+	public KlantDaoInterface createKlantDao() {
+		// TODO Auto-generated method stub
+		return new KlantDaoSql();
+	}
+
+	@Override
+	public ProductDaoInterface createProductDao() {
+		// TODO Auto-generated method stub
+		return new ProductDaoSql();
+	}
+
+}
