596e62d971b578fc246c821437a8e6d46d693cd2
Unknown <jdelozanne@gmail.com>
2017-11-01 10:05:19 +0100

    paar kleine debugs in invoer mbt nextLine() en bestelling.klantID

---
+++ src/main/java/kiteshop/controller/BestelRegelController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kiteshop.controller;
+
+import java.util.logging.Logger;
+import kiteshop.daos.mysql.BestelRegelDaoSql;
+import kiteshop.pojos.BestelRegel;
+import kiteshop.test.ProjectLog;
+import kiteshop.daos.BestelRegelDaoInterface;
+
+/**
+ *
+ * @author julia
+ */
+public class BestelRegelController {
+
+}
