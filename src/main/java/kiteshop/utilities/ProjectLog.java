541f102b7cc15fcefa286676e73a3ad6ef0a83ef
SteefP <s.pelgrom@hotmail.com>
2017-11-08 11:11:32 +0100

    hopelij nu gen foutmeldingen logger

--- src/main/java/kiteshop/utilities/ProjectLog.java
+++ src/main/java/kiteshop/utilities/ProjectLog.java
@@ -10,39 +10,43 @@ import java.util.logging.SimpleFormatter;
 
 public class ProjectLog {
 
-	
+
 	/* 
 	 * De 2 waarden hieronder kunnen naar believen worden aangepast door de programmeur
 	 * voor test doeleinden SP SP
-	*/ 
+	 */ 
 	static Level consolePrintLevel = Level.SEVERE;
 	static Level filePrintLevel  = Level.FINEST;
-	
 
-	
+
+
 	public static Logger getLogger(){
 
+		System.out.println("lalala");
 		Logger logr = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);  // zodat hij in het hele project gebruikt kan worden
 		LogManager.getLogManager().reset();
 
 		logr.setLevel(Level.ALL);
-		
+
 		ConsoleHandler ch = new ConsoleHandler();
 		ch.setLevel(consolePrintLevel);  
 		logr.addHandler(ch);
 
+		/*
 		try {
 			//src/main/java/kiteshop/test/ProjectLog.log
-			FileHandler fh = new FileHandler("src/main/java/kiteshop/test/ProjectLog.log", true);
+			/*FileHandler fh = new FileHandler("src/main/java/kiteshop/test/ProjectLog.log", true);
 			fh.setLevel(filePrintLevel);
 			fh.setFormatter(new SimpleFormatter());
 
 			logr.addHandler(fh);
 
+
+			sout
 		} catch (IOException e){
 			logr.log(Level.SEVERE, "File logger not working", e);
 		}
-
+		 */
 		return logr;
 	}
 	/*
