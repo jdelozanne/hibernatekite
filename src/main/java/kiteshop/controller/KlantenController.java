f98198e677daec1f0cbb0d3c4e1e29fd24873be7
SteefP <s.pelgrom@hotmail.com>
2017-11-20 12:35:23 +0100

    dao uit controller

--- src/main/java/kiteshop/controller/KlantenController.java
+++ src/main/java/kiteshop/controller/KlantenController.java
@@ -3,8 +3,6 @@ package kiteshop.controller;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
-
-import kiteshop.daos.mysql.KlantDaoSql;
 import kiteshop.pojos.Klant;
 import kiteshop.utilities.ProjectLog;
 import kiteshop.daos.KlantDaoInterface;
